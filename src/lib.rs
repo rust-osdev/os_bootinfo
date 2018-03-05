7121206ed2dd44b098d34905b8587441bccc54e5
Philipp Oppermann <dev@phil-opp.com>
2018-03-05 22:10:50 +0100

    Make BootInfo repr(C) and check it on compilation

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
+#![deny(improper_ctypes)]
 #![no_std]
 
 extern crate x86_64;
-extern crate arrayvec;
 
-use x86_64::PhysAddr;
+pub use memory_map::*;
+
 use x86_64::structures::paging::PageTable;
-use arrayvec::ArrayVec;
 
-pub type MemoryMap = ArrayVec<[MemoryRegion; 32]>;
+mod memory_map;
 
 #[derive(Debug)]
+#[repr(C)]
 pub struct BootInfo {
     pub memory_map: MemoryMap,
     pub p4_table: &'static mut PageTable,
@@ -28,52 +29,6 @@ impl BootInfo {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct MemoryRegion {
-    pub start_addr: PhysAddr,
-    pub len: u64,
-    pub region_type: MemoryRegionType
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum MemoryRegionType {
-    /// free RAM
-    Usable,
-    /// used RAM
-    InUse,
-    /// unusable
-    Reserved,
-    /// ACPI reclaimable memory
-    AcpiReclaimable,
-    /// ACPI NVS memory
-    AcpiNvs,
-    /// Area containing bad memory
-    BadMemory,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(C)]
-pub struct E820MemoryRegion {
-    pub start_addr: u64,
-    pub len: u64,
-    pub region_type: u32,
-    pub acpi_extended_attributes: u32,
-}
-
-impl From<E820MemoryRegion> for MemoryRegion {
-    fn from(region: E820MemoryRegion) -> MemoryRegion {
-        let region_type = match region.region_type {
-            1 => MemoryRegionType::Usable,
-            2 => MemoryRegionType::Reserved,
-            3 => MemoryRegionType::AcpiReclaimable,
-            4 => MemoryRegionType::AcpiNvs,
-            5 => MemoryRegionType::BadMemory,
-            t => panic!("invalid region type {}", t),
-        };
-        MemoryRegion {
-            start_addr: PhysAddr::new(region.start_addr),
-            len: region.len,
-            region_type
-        }
-    }
+extern "C" {
+    fn _improper_ctypes_check(_boot_info: BootInfo);
 }
---
+++ src/memory_map.rs
@@ -0,0 +1,103 @@
+use core::ops::{Deref, DerefMut};
+use x86_64::PhysAddr;
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct MemoryMap {
+    entries: [MemoryRegion; 32],
+    last_used_entry: u64,
+}
+
+impl MemoryMap {
+    pub fn new() -> Self {
+        MemoryMap {
+            entries: [MemoryRegion::empty(); 32],
+            last_used_entry: 0,
+        }
+    }
+
+    pub fn add_region(&mut self, region: MemoryRegion) {
+        self.entries[self.last_used_entry as usize] = region;
+        self.last_used_entry += 1;
+    }
+}
+
+impl Deref for MemoryMap {
+    type Target = [MemoryRegion];
+
+    fn deref(&self) -> &Self::Target {
+        &self.entries
+    }
+}
+
+impl DerefMut for MemoryMap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.entries
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub struct MemoryRegion {
+    pub start_addr: PhysAddr,
+    pub len: u64,
+    pub region_type: MemoryRegionType
+}
+
+impl MemoryRegion {
+    pub fn empty() -> Self {
+        MemoryRegion {
+            start_addr: PhysAddr::new(0),
+            len: 0,
+            region_type: MemoryRegionType::Reserved,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub enum MemoryRegionType {
+    /// free RAM
+    Usable,
+    /// used RAM
+    InUse,
+    /// unusable
+    Reserved,
+    /// ACPI reclaimable memory
+    AcpiReclaimable,
+    /// ACPI NVS memory
+    AcpiNvs,
+    /// Area containing bad memory
+    BadMemory,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+pub struct E820MemoryRegion {
+    pub start_addr: u64,
+    pub len: u64,
+    pub region_type: u32,
+    pub acpi_extended_attributes: u32,
+}
+
+impl From<E820MemoryRegion> for MemoryRegion {
+    fn from(region: E820MemoryRegion) -> MemoryRegion {
+        let region_type = match region.region_type {
+            1 => MemoryRegionType::Usable,
+            2 => MemoryRegionType::Reserved,
+            3 => MemoryRegionType::AcpiReclaimable,
+            4 => MemoryRegionType::AcpiNvs,
+            5 => MemoryRegionType::BadMemory,
+            t => panic!("invalid region type {}", t),
+        };
+        MemoryRegion {
+            start_addr: PhysAddr::new(region.start_addr),
+            len: region.len,
+            region_type
+        }
+    }
+}
+
+extern "C" {
+    fn _improper_ctypes_check(_boot_info: MemoryMap);
+}
