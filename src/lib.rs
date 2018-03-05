b3d8ec4f55e96631b01db490106abbe1c4d8493e
Philipp Oppermann <dev@phil-opp.com>
2018-03-05 22:11:12 +0100

    Add a version field to boot info

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,29 @@ pub use memory_map::*;
 
 use x86_64::structures::paging::PageTable;
 
+const VERSION: u64 = 1;
+
 mod memory_map;
 
 #[derive(Debug)]
 #[repr(C)]
 pub struct BootInfo {
-    pub memory_map: MemoryMap,
+    pub version: u64,
     pub p4_table: &'static mut PageTable,
+    pub memory_map: MemoryMap,
 }
 
 impl BootInfo {
-    pub fn new(p4_table: &'static mut PageTable) -> Self {
+    pub fn new(p4_table: &'static mut PageTable, memory_map: MemoryMap) -> Self {
         BootInfo {
-            memory_map: ArrayVec::new(),
-            p4_table
+            version: VERSION,
+            p4_table,
+            memory_map
         }
+    }
+
+    pub fn check_version(&self) -> Result<(), ()> {
+        if self.version == VERSION { Ok(()) } else { Err(()) }
     }
 
     pub fn sort_memory_map(&mut self) {
