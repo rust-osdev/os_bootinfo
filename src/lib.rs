f0ced4843eafe5eba12ce4948daed18a0c5eaa51
Lachlan Sneff <lachlan.sneff@gmail.com>
2018-08-13 08:27:00 -0400

    Add package and package memory type (#5)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 #![deny(improper_ctypes)]
 #![no_std]
 
+use core::ops::Deref;
+use core::slice;
 pub use memory_map::*;
 
-const VERSION: u64 = 4;
+const VERSION: u64 = 5;
 
 mod memory_map;
 
@@ -13,14 +15,33 @@ pub struct BootInfo {
     pub version: u64,
     pub p4_table_addr: u64,
     pub memory_map: MemoryMap,
+    pub package: Package,
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct Package {
+    ptr: *const u8,
+    len: u64,
+}
+
+impl Deref for Package {
+    type Target = [u8];
+    fn deref(&self) -> &[u8] {
+        unsafe { slice::from_raw_parts(self.ptr, self.len as usize) }
+    }
 }
 
 impl BootInfo {
-    pub fn new(p4_table_addr: u64, memory_map: MemoryMap) -> Self {
+    pub fn new(p4_table_addr: u64, memory_map: MemoryMap, package: &'static [u8]) -> Self {
         BootInfo {
             version: VERSION,
             p4_table_addr,
             memory_map,
+            package: Package {
+                ptr: package.as_ptr(),
+                len: package.len() as u64,
+            },
         }
     }
 
